14d78bf3af15d9f4d286a2be658f4d57cc9cf7ef
Milind Jha <milindjha@Milinds-MacBook-Air.local>
2024-12-22 13:26:44 +0530

    first commit

---
+++ src/main/java/com/milind/journalismservice/JournalismServiceApplication.java
@@ -0,0 +1,13 @@
+package com.milind.journalismservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalismServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalismServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milind/journalismservice/JournalismServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milind.journalismservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalismServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
